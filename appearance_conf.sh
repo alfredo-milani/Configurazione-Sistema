465e493c5527f29a0a836872bee1450254ce4414
Alfredo Milani <alfredo.milani.94@gmail.com>
2017-07-09 03:33:53 +0200

    minor fix

--- appearance_conf.sh
+++ appearance_conf.sh
@@ -19,12 +19,45 @@ str_end="${Y}--${NC}$mod_end $mod_\n";
 
 
 
+# funzione per decomprimere archivi
+# argomento #1 --> path di locazione
+# argomento #2 --> nome file
+function extract_files {
+	if [ -f "$1/$2" ] && ! [ -d "$1/$2" ]; then
+		tmp="${2##*.}";
+		case "$tmp" in
+			tar ) 	tar -xf "$1/$2" -C "$1" &> $null ;;
+			xz )	tar -xvf "$1/$2" -C "$1" &> $null ;;
+			gz )	tar -zxvf "$1/$2" -C "$1" &> $null ;;
+			bz2 )	tar -jxvf "$1/$2" -C "$1" &> $null ;;
+			zip ) 	unzip "$1/$2" -d "$1" &> $null ;;
+			7z ) 	7z x "$1/$2" -o"$1" &> $null ;;
+			* )
+					printf "${R}Formato sconosciuto: $tmp. Estrazione non riuscita.\n${NC}";
+					return $EXIT_FAILURE;
+		esac
+
+		printf "Vuoi rimuovere il file compresso?\n$choise_opt";
+		read choise;
+		[ "$choise" == "y" ] && rm -rf "$1/$2";
+	else
+		printf "${R}Errore. Il file: $2 non esiste\n${NC}";
+		return $EXIT_FAILURE;
+	fi
+
+	return $EXIT_SUCCESS;
+}
+
 printf "Vuoi configurare il tema GTK+ del sistema?\n$choise_opt";
 read choise;
 if [ "$choise" == "y" ] && check_tool "gsettings" && check_mount $UUID_backup; then
-	theme_scelto="T4G_3.0_theme";
+	theme_scelto="T4G_3.0_theme.tar.xz";
 	path_backup_theme=$mount_point/$tree_dir/$themes_backup;
 	path_sys_theme=/usr/share/themes/;
+
+	! extract_files $path_backup_theme $theme_scelto &&
+	printf "$str_end" && exit $EXIT_FAILURE;
+
 	sudo cp -r $path_backup_theme/$theme_scelto $path_sys_theme;
 	check_error "Copia tema in $path_sys_theme";
 	gsettings set org.gnome.desktop.interface gtk-theme $theme_scelto;
@@ -36,9 +69,13 @@ fi
 printf "Vuoi configurare le icone del sistema?\n$choise_opt";
 read choise;
 if [ "$choise" = "y" ] && check_tool "gsettings" && check_mount $UUID_backup; then
-	icon_scelto="Flat_Remix";
+	icon_scelto="Flat_Remix.tar.xz";
 	path_backup_icon=$mount_point/$tree_dir/$icons_backup;
 	path_sys_icon="/usr/share/icons/";
+
+	! extract_files $path_backup_icon $icon_scelto &&
+	printf "$str_end" && exit $EXIT_FAILURE;
+
 	sudo cp -r $path_backup_icon/$icon_scelto $path_sys_icon;
 	check_error "Copia set di icone in $path_sys_icon";
 	gsettings set org.gnome.desktop.interface icon-theme $icon_scelto;
