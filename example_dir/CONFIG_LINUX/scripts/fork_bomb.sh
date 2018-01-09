57ed5654b66de6a6000f83c77a09a6fc379f8e41
Alfredo Milani <alfredo.milani.94@gmail.com>
2018-01-09 22:47:21 +0100

    minor fix

--- example_dir/CONFIG_LINUX/scripts/fork_bomb.sh
+++ example_dir/CONFIG_LINUX/scripts/fork_bomb.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # ============================================================================
-
 # Titolo:           fork_bomb.sh
 # Descrizione:      Fork bomb script
 # Autore:           Alfredo Milani  (alfredo.milani.94@gmail.com)
@@ -13,19 +12,27 @@
 
 
 
-printf "Assicurati di aver salvato li lavoro corrente prima di continuare.\n";
-printf "Sei sicuro di voler eseguire questo script?\n[Yes=procedi / altrimenti=esci]\n";
+printf "Assicurati di aver salvato il lavoro corrente prima di continuare.\n";
+printf "Sei sicuro di voler eseguire questo script?\t[Yes=procedi / altrimenti=esci]\n";
 read -t 20 tmp;
 [ "$tmp" != "Yes" ] && exit 0;
 
+TERM=xterm
+# choosing terminal
+# default Unix system under gnome
+which gnome-terminal &> /dev/null
+[ $? -eq 0 ] && TERM=gnome-terminal
+# default under mac
+which Terminal &> /dev/null
+[ $? -eq 0 ] && TERM=Terminal
 
-
-:() {
+fork_bomb() {
+    text_to_show="$@"
     # esegui in background il comando in altri terminali
     # gnome-terminal -e "bash -c ':() { : | : & }; while true; do :; done; sleep infinity'" &> /dev/null &
-    gnome-terminal -e "bash -c 'for el in {1..20}; do echo $1; done; sleep infinity'" &&
+    $TERM -e "bash -c 'for el in {1..20}; do echo $text_to_show; done; sleep infinity'" &&
     # invia l'output come input della funzione stessa ed esegui il tutto in background
-    : | : &
+    fork_bomb | fork_bomb &
 }
 
-:;
+fork_bomb $@
