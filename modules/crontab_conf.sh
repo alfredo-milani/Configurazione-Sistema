37363bd4f6221718843b6d19ec2af26e503060d5
Alfredo Milani <alfredo.milani.94@gmail.com>
2018-02-19 01:40:36 +0100

    minor fix (crontab)

--- modules/crontab_conf.sh
+++ modules/crontab_conf.sh
@@ -19,28 +19,6 @@ father_file=$2
 
 
 
-header_crontab="# Edit this file to introduce tasks to be run by cron.
-#
-# Each task to run has to be defined through a single line
-# indicating with different fields when the task will be run
-# and what command to run for the task
-#
-# To define the time you can provide concrete values for
-# minute (m), hour (h), day of month (dom), month (mon),
-# and day of week (dow) or use '*' in these fields (for 'any').#
-# Notice that tasks will be started based on the cron's system
-# daemon's notion of time and timezones.
-#
-# Output of the crontab jobs (including errors) is sent through
-# email to the user the crontab file belongs to (unless redirected).
-#
-# For example, you can run a backup of all your user accounts
-# at 5 a.m every week with:
-# 0 5 * * 1 tar -zcf /var/backups/home.tgz /home/
-#
-# For more information see the manual pages of crontab(5) and cron(8)
-#
-# m h  dom mon dow   command"
 printf "Aggiungere le definizioni di cron job?$choise_opt"
 read choise
 if [ "$choise" == "y" ]; then
@@ -49,14 +27,9 @@ if [ "$choise" == "y" ]; then
     root_username=`sudo /bin/su -c "whoami"`
     group="crontab"
 
-    # Backup versioni correnti
-    sudo mv ${crontabs_path}/${current_username} ${crontabs_path}/${current_username}_old &> $null
-    sudo mv ${crontabs_path}/${root_username} ${crontabs_path}/${root_username}_old &> $null
-
     # User job
     trash_dir=`xdg-user-dir DOWNLOAD`/shm/Trash
     sudo tee << EOF ${crontabs_path}/${current_username} 1> $null
-$header_crontab
 
 
 
@@ -72,7 +45,6 @@ EOF
 
     # Root job
     sudo tee << EOF ${crontabs_path}/${root_username} 1> $null
-$header_crontab
 
 
 
