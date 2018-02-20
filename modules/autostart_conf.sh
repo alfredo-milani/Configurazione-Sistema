bd26ca0be57a8acd7ab3d5fc5b1bd62a68f116cb
Alfredo Milani <alfredo.milani.94@gmail.com>
2018-02-20 01:37:06 +0100

    minor fix

--- modules/autostart_conf.sh
+++ modules/autostart_conf.sh
@@ -22,73 +22,93 @@ path_as="/etc/xdg/autostart"
 printf "Vuoi disabilitare l'avvio automatico di tracker* tools?\n$choise_opt"
 read choise
 if [ "$choise" == "y" ]; then
-	files_da_modificare="tracker-*"
+	file="tracker-*"
 
 	<<COMM
 	old_str="X-GNOME-Autostart-enabled=true"
 	new_str="X-GNOME-Autostart-enabled=false"
-	for file in "$path_as/"$files_da_modificare; do
+	for file in "$path_as/"$file; do
 		sudo sed -i "s/$old_str/$new_str/g" "$file"
 		sudo tee -a <<< `printf "\nHidden=true\n"` "$file"
 	done
 COMM
 
-	for file in "$path_as/"$files_da_modificare; do
+	for file in "$path_as/"$file; do
 		sudo mv "$file" "$file"_old
 	done
-	check_error "Modifica files $files_da_modificare in $path_as"
+	check_error "Modifica files $file in $path_as"
 
 	gsettings set org.freedesktop.Tracker.Miner.Files crawling-interval -2
 	gsettings set org.freedesktop.Tracker.Miner.Files enable-monitors false
 	tracker reset --hard
-	echo "Lancio del comando 'tracker-preferences' per disabilitare $files_da_modificare completamente"
+	echo "Lancio del comando 'tracker-preferences' per disabilitare $file completamente"
 	tracker-preferences &> $null
 
 	# riavvio richiesto
 	reboot_req "$father_file"
 else
-	printf "${DG}${U}$files_da_modificare tools non disabilitato${NC}\n"
+	printf "${DG}${U}$file tools non disabilitato${NC}\n"
 fi
 
 printf "Vuoi disabilitare l'avvio automatico di orca tool?\n$choise_opt"
 read choise
 if [ "$choise" == "y" ]; then
-	files_da_modificare="orca-autostart.desktop"
+	file="orca-autostart.desktop"
 
 	<<COMM
 	old_str="NoDisplay=true"
 	new_str="NoDisplay=false"
-	sudo sed -i "s/$old_str/$new_str/g" "$path_as/$files_da_modificare"
+	sudo sed -i "s/$old_str/$new_str/g" "$path_as/$file"
 	check_error "Modifica files orca-autostart #1 in $path_as"
 
 	old_str="X-GNOME-AutoRestart=true"
 	new_str="X-GNOME-AutoRestart=false"
-	sudo sed -i "s/$old_str/$new_str/g" "$path_as/$files_da_modificare"
+	sudo sed -i "s/$old_str/$new_str/g" "$path_as/$file"
 	check_error "Modifica files orca-autostart #2 in $path_as"
 COMM
 
-	sudo mv "$path_as/$files_da_modificare" "$path_as/$files_da_modificare"_old
-	check_error "Modifica files $files_da_modificare in $path_as"
+	sudo mv "$path_as/$file" "$path_as/$file"_old
+	check_error "Modifica files $file in $path_as"
 
 	# riavvio richiesto
 	reboot_req "$father_file"
 else
-	printf "${DG}${U}$files_da_modificare tools non disabilitato${NC}\n"
+	printf "${DG}${U}$file tools non disabilitato${NC}\n"
 fi
 
 printf "Vuoi disabilitare l'avvio automatico di caribou tool (tastiera a schermo)?\n$choise_opt"
 read choise
 if [ "$choise" == "y" ]; then
 	# tastiera on screen
-	files_da_modificare="caribou-autostart.desktop"
+	file="caribou-autostart.desktop"
 
-	sudo mv "$path_as/$files_da_modificare" "$path_as/$files_da_modificare"_old
-	check_error "Modifica files $files_da_modificare in $path_as"
+	sudo mv "$path_as/$file" "$path_as/$file"_old
+	check_error "Modifica files $file in $path_as"
 
 	# riavvio richiesto
 	reboot_req "$father_file"
 else
-	printf "${DG}${U}$files_da_modificare tools non disabilitato${NC}\n"
+	printf "${DG}${U}$file tools non disabilitato${NC}\n"
+fi
+
+printf "Vuoi impostare l'avvio automatico del tool redshift?\n$choise_opt"
+read choise
+if [ "$choise" == "y" ]; then
+	file="redshift.desktop"
+
+	sudo tee << EOF "$path_as/$file" 1> $null
+[Desktop Entry]
+Exec=/usr/bin/redshift_regolator.sh %f
+Terminal=false
+Type=Application
+NoDisplay=true
+EOF
+	check_error "Modifica files $file in $path_as"
+
+	# riavvio richiesto
+	reboot_req "$father_file"
+else
+	printf "${DG}${U}$file non impostato per l'autostart${NC}\n"
 fi
 
 
